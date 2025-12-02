682187e68a0e3a3eeadec1465f369cda00820531
Nivedit Gupta <niveditgupta@hotmail.co.uk>
2025-12-02 02:04:23 +0000

    change 5

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const APP_NAME = appName || 'dinodia-mobile';
+
+AppRegistry.registerComponent(APP_NAME, () => App);
+
+if (APP_NAME !== 'dinodia-mobile') {
+  AppRegistry.registerComponent('dinodia-mobile', () => App);
+}
